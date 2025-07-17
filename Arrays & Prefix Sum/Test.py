045e2fbd525542b655c5e94d7ade9b622f238f41
Abhay Singh <abhaysingh123@gmail.com>
2025-07-17 18:33:01 +0530

    made some new changes

--- Arrays & Prefix Sum/Test.py
+++ Arrays & Prefix Sum/Test.py
@@ -1,10 +1,10 @@
-class solution():
-    def two_sum(self, nums, target):
-        num_map = {}
-        for i, num in enumerate(nums):
-            complement = num - target
-            if complement in num_map:
-                return[num[complement], i]
+# class solution():
+#     def two_sum(self, nums, target):
+#         num_map = {}
+#         for i, num in enumerate(nums):
+#             complement = num - target
+#             if complement in num_map:
+#                 return[num[complement], i]
 
 # buy and sell stocks 
 # Step 1 := Frist make a array that will take input of the int as an array.
@@ -23,6 +23,32 @@ class solution():
 # Return the max profit
  
 
+
+'''
+Two pointers question
+Given a sorted list of integers, remove the duplicates in-place such that each element appears only once
+and return the new length.
+Do not use extra space for another list — 
+you must do this by modifying the original list in-place with O(1) extra memory.
+'''
+# input_num = list(map(int, input().split()))
+# for element in input_num(num):
+#     if num in input_num:
+#         return input_num
+#     else:
+
+
+nums = list(map(int, input("Enter sorted numbers: ").split()))
+
+i = 0  # This is where we place the next unique number
+for j in range(1, len(nums)):
+    if nums[j] != nums[i]:  # Found a new unique number
+        i += 1              # Move i to next position
+        nums[i] = nums[j]   # Overwrite it with the new unique number
+
+# i + 1 is the length of unique elements
+print("Length after removing duplicates:", i + 1)
+print("Modified list:", nums[:i+1])
 
 
 
---
+++ Basic_programme/test.py
@@ -0,0 +1,101 @@
+# class solution:
+#     def sum_of_digit(self, num: int) ->int:
+#         total = 0
+#         while num > 0 :
+#             total += num % 10
+#             num //= 10
+#         return total
+
+
+
+# class solution:
+#     def reverse_digit(self, num: int) ->int
+#     for i in range(num):
+#         reverse = num % 10
+#         return reverse
+
+# class solution:
+#     def count_digit(self, num: int):
+#         for i in range(num):
+#             count = num // 10
+
+# class solution:
+#     def count_digit(self, num: int):
+#         while num > 0:
+#             count = num // 10
+#             count +1
+
+# class solution:
+#     def count_digit(self, num: int):
+#         count = 0
+#         if num == 0:
+#             return 1
+        
+#     while num > 0:
+#         num = num // 10
+#         count +=1
+#     return count    
+
+# for 
+
+# class solution:
+#     def sum_of_digit(self, num: int):
+#         sum = 0 
+#         while num > 0:
+#             num = num // 10
+#             sum += 1
+#         return sum
+
+
+'''Q4. Check for Palindrome Number
+Check if the number reads the same forward and backward.
+Example: Input: 121 → Output: True, Input: 123 → Output: False
+'''
+# class solution:
+#     def palindrome(self, num: int):
+#         # edge case
+#         if num == 0:
+#         #   if the input is 0 this will treat not a plaindrome
+#           return False 
+#         while num > 0:
+#            last_digit = 0
+#            last_digit = num % 10
+           
+"""
+Q5. Find the Largest Digit
+Return the largest digit in a number.
+Example: Input: 4723 → Output: 7
+"""          
+# class solution:
+#     def largest_digit(self, num: int):
+#         if num == 0:
+#             return 0
+#         while num > 0:
+#             # extract every digit from the number 
+#             # and then campare then in this while loop
+#             digit = num % 10
+
+'''
+Q6. Sum of Even Digits
+Take a number and return the sum of only the even digits.
+Example: Input: 123456 → Output: 2 + 4 + 6 = 12
+'''
+class solution:
+    def sum_of_even_digit(self, num: int):
+        # take out the single digit from the number 
+        # check the divisiblity by 2 
+        # if the remainder is 0 and add them
+        while num > 0:
+            single_digit = num % 10
+            check_even = single_digit / 2
+            if check_even == 0:
+                # add all the digit with reminder 0 
+                # but i dont know how to code it
+            else:
+                # this is the case of odd digit in the number leave this number
+                # do nothing here
+                # break
+                return check_even
+
+
+
